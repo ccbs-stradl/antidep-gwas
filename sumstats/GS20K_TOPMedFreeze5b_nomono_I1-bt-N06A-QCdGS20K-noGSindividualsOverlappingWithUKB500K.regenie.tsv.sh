b354e3b1879a2247ead6433cfc2fa6ad2028a6e3
madams23 <mark.adams@ed.ac.uk>
2023-07-25 14:28:10 +0100

    exposure phenotypes for ATC categories (N06A)

---
+++ sumstats/GS20K_TOPMedFreeze5b_nomono_I1-bt-N06A-QCdGS20K-noGSindividualsOverlappingWithUKB500K.regenie.tsv.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+sumstats=$1
+output=$2
+
+# sumstats columns
+#  1  CHROM
+#  2  GENPOS
+#  3  ID
+#  4  ALLELE0
+#  5  ALLELE1
+#  6  A1FREQ
+#  7  A1FREQ_CASES
+#  8  A1FREQ_CONTROLS
+#  9  INFO
+# 10  N
+# 11  N_CASES
+# 12  N_CONTROLS
+# 13  TEST
+# 14  BETA
+# 15  SE
+# 16  CHISQ
+# 17  LOG10P
+# 18  EXTRA
+# 19  RSID
+
+gunzip -c $sumstats | awk 'OFS = "\t" {if(NR == 1) {print "chr", "pos", "ea", "oa", "beta", "se", "pval", "ncase", "ncontrol", "snp", "eaf", "imp_info", "eaf_case", "eaf_control"} else {print "chr"$1, $2, $5, $4, $14, $15, 10^(-$17), $11, $12, $3, $8, $9, $7, $8}}' > $output
+
+# output columns
+# chr
+# pos
+# ea
+# oa
+# beta
+# se
+# pval
+# ncase
+# ncontrol
+# snp
+# eaf
+# imp_info
+# eaf_case
+# eaf_control
