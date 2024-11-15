9cac477f2cc01fcf3a3a410fe4b089098264f8ad
Amelia Edmondson - Stait <aedmond3@node1c32.ecdf.ed.ac.uk>
2024-11-15 15:41:20 +0000

    draft script for running susiex

--- fine_mapping.sh
+++ fine_mapping.sh
@@ -1,25 +1,39 @@
-# Example SuSiEx script to try running then edit:
-../bin/SuSiEx \
-  --sst_file=EUR.sumstats.txt,AFR.sumstats.txt \
+# Test SuSiEx script before adding to nextflow pipeline:
+
+qlogin -l h_vmem=32G
+
+cd /exports/eddie/scratch/aedmond3/GitRepos/antidep-gwas
+
+module load roslin/bcftools/1.20
+module load roslin/samtools/1.9
+
+# Convert sustats into correct format (convert to nextflow process)
+for cluster in EUR AFR SAS; do
+echo -e "SNP\tA1\tA2\tfreq\tBETA\tSE\tP\tN\tCHR\tBP\tNE" > test/${cluster}.sumstats.txt
+bcftools query \
+  -f "%ID\\t%ALT\\t%REF\\t[%AFCON]\\t[%ES]\\t[%SE]\\t[%LP]\\t[%SS]\\t%CHROM\\t%POS\\t[%NE]" liftover/fixed-N06A-${cluster}.human_g1k_v37.vcf.gz >> test/${cluster}.sumstats.txt
+done
+
+# Run SuSiEX (convert to nextflow process)
+../SuSiEx/bin/SuSiEx \
+  --sst_file=EUR.sumstats.txt,AFR.sumstats.txt,SAS.sumstats.txt \
   --n_gwas=50000,50000 \
-  --ref_file=EUR,AFR \
-  --ld_file=EUR,AFR \
+  --ref_file=EUR,AFR,SAS \
+  --ld_file=EUR,AFR,SAS \
   --out_dir=./ \
-  --out_name=SuSiEx.EUR.AFR.output.cs95 \
+  --out_name=SuSiEx.EUR.AFR.SAS.output.cs95 \
   --level=0.95 \
   --pval_thresh=1e-5 \
   --maf=0.005
-  --chr=1 \
-  --bp=7314654,8314677 \
-  --snp_col=2,2 \
-  --chr_col=1,1 \
-  --bp_col=3,3 \
-  --a1_col=4,4 \
-  --a2_col=5,5 \
-  --eff_col=6,6 \
-  --se_col=7,7 \
-  --pval_col=9,9 \
-  --plink=../utilities/plink \
+  --snp_col=1,1,1 \
+  --chr_col=9,9,9 \
+  --bp_col=10,10,10 \
+  --a1_col=2,2,2 \
+  --a2_col=3,3,3 \
+  --eff_col=5,5,5 \
+  --se_col=6,6,6 \
+  --pval_col=7,7,7 \
+  --plink=./gpfs/igmmfs01/eddie/GenScotDepression/amelia/packages/plink2 \
   --mult-step=True \
   --keep-ambig=True \
   --threads=16
