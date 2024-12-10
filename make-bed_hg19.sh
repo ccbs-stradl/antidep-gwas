0ea2953df1e101f2c8d6b4a8e99f3f0d548f16cc
Amelia Edmondson - Stait <aedmond3@login03.ecdf.ed.ac.uk>
2024-12-10 13:53:01 +0000

    use keep-col-match and remove mac maf filtering

--- make-bed_hg19.sh
+++ make-bed_hg19.sh
@@ -17,14 +17,12 @@ cd /exports/eddie/scratch/$USER/GitRepos/antidep-gwas
 # Create a file for each ancestry in UKB (EUR AFR SAS) with columns for UKB ID (FID and IID)
 for cluster in EUR AFR SAS; do
 /gpfs/igmmfs01/eddie/GenScotDepression/amelia/packages/plink2 \
---pfile /exports/igmm/eddie/GenScotDepression/data/ukb/genetics/impv3_pgen/ukb_imp_v3.qc \
---keep /exports/igmm/eddie/GenScotDepression/data/ukb/genetics/input_filters/similarity_clusters/ld_ref/ukb-ld-ref_${cluster}.id \
---maf 0.01 \
---mac 100 \
+--pfile reference/ukb_imp_v3.qc_ancestry \
+--keep-col-match reference/ukb-ld-ref_ancestry.id ${cluster} \
 --geno 0.02 \
 --mind 0.02 \
 --make-bed \
---out reference/ukb_imp_v3.qc_${cluster} \
+--out reference/ukb_imp_v3.qc_ \
 --threads 4
 done
 
