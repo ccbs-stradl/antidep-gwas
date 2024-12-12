67e446724b6d0664fee9012ba8b0d58f3210045a
Amelia Edmondson - Stait <aedmond3@login03.ecdf.ed.ac.uk>
2024-12-12 17:10:23 +0000

    filter by geno before mind

--- make-bed_hg19.sh
+++ make-bed_hg19.sh
@@ -17,15 +17,20 @@
 cd /exports/eddie/scratch/$USER/GitRepos/antidep-gwas
 
 # Create a file for each ancestry in UKB (EUR AFR SAS) with columns for UKB ID (FID and IID)
-for cluster in EUR AFR SAS; do
+# Too many individuals were dropped when mind was 0.02, so try filtering geno first then mind
 /gpfs/igmmfs01/eddie/GenScotDepression/amelia/packages/plink2 \
 --pfile /exports/igmm/eddie/GenScotDepression/data/ukb/genetics/impv3_pgen/ukb_imp_v3.qc \
 --keep-col-match reference/ukb-ld-ref_ancestry.id $cluster \
 --geno 0.02 \
+--make-pgen 'vzs' \
+--out reference/ukb_imp_v3.qc.geno02_${cluster} \
+--threads 4
+
+/gpfs/igmmfs01/eddie/GenScotDepression/amelia/packages/plink2 \
+--pfile reference/ukb_imp_v3.qc.geno02_${cluster} 'vzs' \
 --mind 0.02 \
 --make-bed \
---out reference/ukb_imp_v3.qc_${cluster} \
+--out reference/ukb_imp_v3.qc.geno02.mind02_${cluster} \
 --threads 4
-done
 
 
