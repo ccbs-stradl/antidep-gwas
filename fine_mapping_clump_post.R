d3a4c80c0c6061a023d228f0a953f6f1c819994f
AmeliaES <amelia.edmondson-stait@ed.ac.uk>
2025-02-05 16:00:25 +0000

    remove any NULL values from list to ensure GRanges error doesn't occur again

--- fine_mapping_clump_post.R
+++ fine_mapping_clump_post.R
@@ -18,14 +18,14 @@
   # The first argument is the nested_clumps string
   nested_clumps <- args[1]
 
-  nested_clumps <- "[[EUR, fixed, N06A, /exports/eddie/scratch/aedmond3/ad/work/ca/f5a554f9730a7e45262a0815630bbf/fixed-N06A-EUR.ma, /exports/eddie/scratch/aedmond3/ad/work/ca/f5a554f9730a7e45262a0815630bbf/fixed-N06A-EUR.3.clumps, /exports/eddie/scratch/aedmond3/ad/work/ca/f5a554f9730a7e45262a0815630bbf/fixed-N06A-EUR.3.log, 3], [AFR, fixed, N06A, /exports/eddie/scratch/aedmond3/ad/work/69/2731963a60ec1c8d7c77acddd5b7b4/fixed-N06A-AFR.ma, /exports/eddie/scratch/aedmond3/ad/work/69/2731963a60ec1c8d7c77acddd5b7b4/fixed-N06A-AFR.3.clumps, /exports/eddie/scratch/aedmond3/ad/work/69/2731963a60ec1c8d7c77acddd5b7b4/fixed-N06A-AFR.3.log, 3], [SAS, fixed, N06A, /exports/eddie/scratch/aedmond3/ad/work/a1/56c1329b83ed8e0edf449a317ca361/fixed-N06A-SAS.ma, /exports/eddie/scratch/aedmond3/ad/work/a1/56c1329b83ed8e0edf449a317ca361/fixed-N06A-SAS.3.clumps, /exports/eddie/scratch/aedmond3/ad/work/a1/56c1329b83ed8e0edf449a317ca361/fixed-N06A-SAS.3.log, 3]]"
-
-  cat(nested_clumps)
-
+  # Remove the inner and outer "[[" and "]]"
   nested_clumps_trim <- str_remove_all(nested_clumps, "\\[\\[|\\]\\]")
 
+  # Create a list of the collected channels, so each item in the list is a channel before they were collected here:  CLUMP_CLUSTER_CH = CLUMP_CH.collect(flat : false)
+  # each item in the list corresponds to a different ancestry
   nested_clumps_list <- as.list(unlist(str_split(nested_clumps_trim, "\\], \\[")))
 
+  # clean up white space and spit each item in the list by "," into a vector
   nested_clumps_clean <- lapply(nested_clumps_list, function(l){
                         str_split(l, ",") %>%
                         unlist() %>%
@@ -33,17 +33,18 @@
                         str_remove_all(., "\\'")
                       })
 
-  # Get details on CHR
+  # Get details on CHR (7th item in channel "CLUMP_CH" - this code will break if the output order of CLUMP_CH changes)
   chr <- unique ( sapply(nested_clumps_clean, function(l){ l[7] }) )
 
-  # start lapply here over nested_clumps_clean
+  # start lapply here over nested_clumps_clean, get a list of GRanges for each ancestry
+  # return NULL if there is no GRanges object
   granges_list <- lapply(nested_clumps_clean, function(nested_clump_clean){
 
     # Read in clumps results
     clumped_data <- fread(nested_clump_clean[5]) # index 5 is the .clumps file (see order in output from CLUMP process)
 
     if(nrow(clumped_data) == 0){
-      # skip this and go to next element in lapply
+      # skip this and go to next element in lapply if there is no clumped data
       return() # returns NULL
     } else {
 
@@ -73,70 +74,29 @@
 
   })
 
-  granges_list
-
+  # Reduce list of granges into one granges object, then reduce any overlapping regions (from different ancestries)
+  # First deal with all NULLs (ie. no clumps data, and no regions to fine map for all ancestries)
   if (all(sapply(granges_list, is.null))) {
     # If the list is all NULL, create a file with NULL content
     writeLines("NULL", paste0(chr, ".finemapRegions"))
   } else {
 
+  # If the first item in granges_list is a GRanges object then the following code runs:
+  # do.call(c, granges_list) %>%
+  #                 reduce() 
+  # However, if the first item in the list is NULL then that code will result in an error
+  # granges_list_null_first <- list(granges_list[[2]], granges_list[[1]], granges_list[[3]])
+  #   do.call(c, granges_list_null_first) %>%
+  #                   reduce()
+  # Error in (function (classes, fdef, mtable)  : 
+  # unable to find an inherited method for function ‘reduce’ for signature ‘"list"’
 
-# # Test code to get overlapping regions for multiple ancestries
-# gr1 <- GRanges(
-#   seqnames = "1", 
-#   ranges = IRanges(start = 100, end = 200), 
-#   strand = "*", 
-#   SNP = "rs111111"
-# )
-# gr2 <- GRanges(
-#   seqnames = "1", 
-#   ranges = IRanges(start = 100, end = 200), 
-#   strand = "*", 
-#   SNP = "rs111112"
-# )
-# # Example 2: Non-Overlapping Ranges
-# gr3 <- GRanges(
-#   seqnames = "1", 
-#   ranges = IRanges(start = 300, end = 400), 
-#   strand = "*", 
-#   SNP = "rs222222"
-# )
-# gr4 <- GRanges(
-#   seqnames = "1", 
-#   ranges = IRanges(start = 500, end = 600), 
-#   strand = "*", 
-#   SNP = "rs222223"
-# )
-# # Example 3: Partly Overlapping Ranges
-# gr5 <- GRanges(
-#   seqnames = "1", 
-#   ranges = IRanges(start = 150, end = 250), 
-#   strand = "*", 
-#   SNP = "rs333333"
-# )
-# gr6 <- GRanges(
-#   seqnames = "1", 
-#   ranges = IRanges(start = 180, end = 280), 
-#   strand = "*", 
-#   SNP = "rs333334"
-# )
-# # Combine the GRanges objects into a list
-# granges_list_test <- list(gr1, gr2, gr3, gr4, gr5, gr6, NULL)
+  # Therefore remove NULL items from list to avoid this error
+  granges_list <- granges_list[granges_list != "NULL"]
 
-
-  # Reduce list of granges into one granges object, then reduce any overlapping regions (from different ancestries)
-  if(inherits(do.call(c, granges_list),  "GRanges")){
-    grng_streched <- do.call(c, granges_list) %>%
+  # Now there are no NULL items, and at least one GRanges object (because of previous "if (all(sapply(granges_list, is.null)))") we
+  grng_streched <- do.call(c, granges_list) %>%
                     reduce()
-  }else{
-    grng_streched <- do.call(c, granges_list)[[1]] %>%
-                    reduce()
-  }
-
-  # If the above throws an error look into this further, as with test data the following works:
-    # do.call(c, granges_list_test) %>%
-    #                 reduce()
-
 
   # Reduce ranges to collapse overlapping or nearby regions
   grng_reduced <- grng_streched %>%
@@ -151,7 +111,7 @@
   }
 
 
-# Output chr
-writeLines(chr, "chr.txt")
+  # Output chr
+  writeLines(chr, "chr.txt")
   
   
