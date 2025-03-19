d5a0fed387dcb927eeaa4a51635c0d071909837d
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-03-19 13:14:52 +0000

    format from long to wide

--- manuscript/scripts/meta_analysis_cohort_summary_table.R
+++ manuscript/scripts/meta_analysis_cohort_summary_table.R
@@ -10,21 +10,21 @@ library(dplyr) # %>%
 library(tidyr) # pivot_wider
 
 # --------------------------------
-# Helper function to read in files 
+# Helper function to read in files
 read_files <- function(dir_name, pattern){
   # Get full paths
   paths <- list.files(dir_name, pattern = paste0(pattern, ".*\\.csv$"), full.names = TRUE)
-  
+
   # Loop through each file and read it using data.table::fread
   files <- lapply(paths, fread)
-  
+
   # Use file name as list item name
   # Remove dir from file name
   file_names <- basename(paths)
-  
+
   # Remove file extension
-  names(files) <- file_path_sans_ext(file_names) 
-  
+  names(files) <- file_path_sans_ext(file_names)
+
   return(files)
 }
 
@@ -33,43 +33,43 @@ read_files <- function(dir_name, pattern){
 add_name_as_col <- function(dataframe, dataframe_name){
   # add dataframe name as new column
   dataframe$meta_analysis <- dataframe_name
-  
+
   # ensure meta-analysis name is the first column
   dataframe <- dataframe %>%
                 relocate(meta_analysis)
-  
+
   return(dataframe)
 }
 
 # --------------------------------
-# rbind a list of dataframes 
+# rbind a list of dataframes
 rbind_dataframes_list <- function(dataframe_list, cols_to_rm){
 
   # Add item list name as a new column
   dataframe_list_named <- lapply(names(dataframe_list), function(name) {
     add_name_as_col(dataframe_list[[name]], name)  # Apply function with dataframe and its name
   })
-  
-  # rbind list of dataframes 
+
+  # rbind list of dataframes
   summary_table <- do.call(rbind, dataframe_list_named)
-  
+
   # Remove cols we don't need (dataset & build)
   summary_table <- summary_table %>%
     select(-all_of(cols_to_rm))
 
   return(summary_table)
-  
+
 }
 
 # --------------------------------
 # save csv
 save_csv <- function(table, summary_tables_path, meta_type){
-  
+
   # Check path exists, other exit with error
   if(!dir.exists(summary_tables_path)){
     stop(paste0("Error: ", summary_tables_path, " directory does not exist."))
   }
-  
+
   write.csv(table, paste0(summary_tables_path, "/meta_analysis_cohort_summary_table_", meta_type, ".csv"), quote = F, row.names = F)
 }
 
@@ -77,44 +77,54 @@ save_csv <- function(table, summary_tables_path, meta_type){
 create_table_MRMEGA <- function(summary_table_path){
   # Read in MR-MEGA cohort csv files
   dataframes <- read_files("meta", "mrmega")
-  
+
   # Concatenate the files together (rbind) and add column for the file name
   summary_table <- rbind_dataframes_list(dataframes, c("dataset", "build"))
-  
-  # Write csv
-  save_csv(summary_table, summary_table_path, "mrmega")
 
-  return(summary_table)
+  # Make the table wider, with columns for cases, controls, and neff for each cohort
+  # Keep ancestry as long format
+  summary_table_wide <- summary_table %>%
+                          select(-c(version, pheno)) %>%
+                          pivot_wider(.,
+                                    names_from = cohort,
+                                     values_from = c(cases, controls, neff)) %>%
+    rename_with(~ gsub("(.+)_(.+)", "\\2_\\1", .), -c(meta_analysis, cluster)) %>%  # Reorder names but exclude "meta_analysis" and "cluster"
+    select(c(meta_analysis, cluster), order(colnames(.)[-c(1:2)]) + 2)  # Keep "meta_analysis" and "cluster" first and reorder the rest
+
+  # Write csv
+  save_csv(summary_table_wide, summary_table_path, "mrmega")
+
+  return(summary_table_wide)
 }
 
 # --------------------------------
 create_table_fixed <- function(summary_table_path){
   # Read in files
   dataframes <- read_files("meta", "fixed")
-  
+
   # Concatenate the files together (rbind) and add column for the file name
-  summary_table <- rbind_dataframes_list(dataframes, c("dataset", "build", "pheno", "version", "cluster")) 
-  
+  summary_table <- rbind_dataframes_list(dataframes, c("dataset", "build", "pheno", "version", "cluster"))
+
   summary_table_wide <- pivot_wider(summary_table,
                                     names_from = cohort,
                                     values_from = c(cases, controls, neff)) %>%
     rename_with(~ gsub("(.+)_(.+)", "\\2_\\1", .), -meta_analysis) %>%  # Reorder names but exclude "meta_analysis"
     select(meta_analysis, order(colnames(.)[-1]) + 1)  # Keep "meta_analysis" first and reorder the rest
-  
+
   # Write csv
   save_csv(summary_table_wide, summary_table_path, "fixed")
-  
+
   return(summary_table_wide)
 }
 
 # --------------------------------
 # Create tables and save them as csv files
 create_summary_tables <- function(summary_table_path){
-  
+
   create_table_MRMEGA(summary_table_path)
-  
-  create_table_fixed(summary_table_path)
-  
+
+  # create_table_fixed(summary_table_path)
+
 }
 
 # --------------------------------
