0bd4fe435398a0c3acf5df9526afd14faadd1a9c
AmeliaES <amelia.edmondson-stait@ed.ac.uk>
2025-03-27 12:09:26 +0000

    also add sentences for mr-mega

--- manuscript/scripts/sentence_for_meta_analysis_results.R
+++ manuscript/scripts/sentence_for_meta_analysis_results.R
@@ -4,6 +4,7 @@
 # AFRcases = X, AFRcontrols = ; EURcases = X, EURcontrols = …."
 
 library("rjson")
+library(stringr)
 
 json_files <- list.files("vcf/meta/GRCh38",
            pattern = "N06A-.*\\.json$",
@@ -20,3 +21,23 @@ lapply(json_data, function(json){
   N <- round(neff/2, 3)
   paste0(cluster, " cases = ",N , ", ", cluster, " controls = ", N, "; ")
 }) %>% do.call(paste0, .)
+
+# -----------------------------------------
+# For the MR-MEGA numbers read in Neff from the cohort summary tables 
+# (can also do this for fixed as it will return the same number)
+
+summary_table_path <- here::here("manuscript", "tables", "meta_analysis_cohort_summary_table_mrmega.csv")
+summary_table <- read.csv(summary_table_path)
+summary_table_neff <- summary_table %>%
+  rowwise() %>%
+  mutate(total_neff = round(sum(c_across(ends_with("neff")), na.rm = TRUE)/2),3) %>%
+  ungroup() %>%
+  select(meta_analysis,cluster, total_neff) %>%
+  filter(str_detect(meta_analysis,  "N06A$")) 
+
+lapply(1:nrow(summary_table_neff), function(i){
+  cluster <- summary_table_neff$cluster[i]
+  N <- summary_table_neff$total_neff[i]
+  paste0(cluster, " cases = ", N , ", ", cluster, " controls = ", N, "; ")
+}) %>% do.call(paste0, .)
+
