4960e0bb965a38765f0f95901fcc015794512a7d
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-04-22 15:23:36 +0100

    refactor, sidecar .cols file paired with results file

--- manuscript/scripts/supplementary_tables_excell.R
+++ manuscript/scripts/supplementary_tables_excell.R
@@ -1,4 +1,4 @@
-# Create supplementary tables in excell spreadsheets for the following:
+# Create supplementary tables in excel spreadsheets for the following:
 # Clumping / finemapping
 # Gene mapping mBAT, cross method
 # GWAS catalog
@@ -17,13 +17,11 @@ library(tidyr)
 library(stringr)
 library(tools)
 
-# Load in functions
-source(here("manuscript/scripts/supplementary_tables_excell_functions.R"))
+# Load in functions (move below functions to this script when finished)
+source(here("manuscript/scripts/supplementary_tables_excel_functions.R"))
 
-###############################################
-#### Meta-analysis and fine mapping table #####
-###############################################
-# Update table index
+# ---------------------------------------------
+# Function to update the counter of the supplementary table eg. S1, S2, S3
 update_table_index <- function(table_index){
   # check table_index is numeric
   if(!is.numeric(table_index)){
@@ -33,73 +31,109 @@ update_table_index <- function(table_index){
   return(table_index)
 }
 
-# Create excell table with 
-# readme as the first sheet with table legend
-# clumping tables
-# fine mapping tables for significant results
-
+# ---------------------------------------------
 # function to get file names from a regular expression string
 # function takes:
 # a string for the path where csv/tsv files are located
 # a string to match file name
-get_file_names <- function(path, file_regex){
+# function returns the .csv or .tsv file names, removes .cols but checks they exist
+get_main_file_names <- function(path, file_regex){
   files <- list.files(path, full.names = TRUE)
-  str_subset(files, file_regex)
-}
-
-# function to read in table of results (csv or tsv)
-# function takes:
-# a string for the full path of csv/tsv file
-read_file <- function(path, file){
-  fread(path)
-}
-
-# function that reads in tables and stores them as a list
-# these tables can then be inserted into the excell sheets
-# function takes:
-# a vector of strings for the full path of csv/tsv files
-tables_list <- function(full_path_vector){
-  tables_list <- lapply(full_path_vector, read_file)
-  # get basename without file extension
-  sheet_name <- file_path_sans_ext(basename(full_path_vector))
-  # sheet name is max 31 characters, if any are above this then truncate them
-  if(any(nchar(sheet_name) > 31)){
-    sheet_name <- str_trunc(sheet_name, 31)
+  files <- str_subset(files, file_regex)
+  
+  # If there are no files stop with error
+  if(length(files) == 0){
+    stop(paste0("No files found at: ", path, " with regex: ", regex))
   }
-  # rename items in list by this name, which will later be the sheet name
-  names(tables_list) <- sheet_name
-  return(tables_list)
+  
+  # If files are found, check if they all end with .csv, .tsv or .cols
+  # If they are not csv or tsv, stop with error
+  if(!any(grepl("\\.csv$", files)) & !any(grepl("\\.tsv$", files)) & !any(grepl("\\.col$", files))){
+    stop(paste0("No csv or tsv files found at: ", path, " with regex: ", regex))
+  }
+  
+  # Check each .csv or .tsv in files has a corresponding .cols file
+  # If not, stop with error
+  for (file in files) {
+    if (grepl("\\.csv$", file) | grepl("\\.tsv$", file)) {
+      cols_file <- paste0(file, ".cols")
+      if (!cols_file %in% files) {
+        stop(paste0("No corresponding .cols file found for: ", file))
+      }
+    }
+  }
+  
+  # Subset to only return .tsv or .csv files
+  files <- str_subset(files, "\\.csv$|\\.tsv$")
+
+  return(files)
 }
 
-# function that gets column names from data frames in the sheets and returns
-# a dataframe with two columns:
-# first column are column names in sheets, second column is a description.
-col_name_descriptions <- function(tables_list, colname_descriptions){
+# ---------------------------------------------
+# Function to read in the results tables and sidecar .cols files
+read_results <- function(full_path){
+  # Read in results tables
+  main <- fread(full_path)
   
-  # Get all unique column names from the list of data frames
-  # Note this assumes the descriptions are the same for duplicate column names
-  # the user needs to check this when they create the descriptions variable
-  col_names <- unique(unlist(lapply(tables_list, colnames)))
+  # Read in sidecar .cols files
+  meta <- fread(paste0(full_path, ".cols"))
   
-  # Create a data frame with column names and match with descriptions
-  col_name_df <- data.frame(
-    `column name` = col_names,
-    `description` = sapply(col_names, function(name) {
-      if (name %in% names(colname_descriptions)) {
-        colname_descriptions[[name]] # get the description that matches the col name
-      } else {
-        ""
-      }
-    }),
-    stringsAsFactors = FALSE
-  )
+  # Combine into a list
+  results <- list(main = main, meta = meta)
   
-  return(col_name_df)
+  return(results)
 }
+
+# ---------------------------------------------
+# function that creates a list of results tables
+# and names them by the file name
+
+tables_list <- function(full_path_vector){
   
-# function that creates README for first sheet in excell spreadsheet
-add_readme <- function(tables_list, wb, table_index, legend_title, legend_text, 
-                       cell_title_width, cell_title_height, colname_descriptions){
+  # Check if full_path_vector is a character vector
+  if(!is.character(full_path_vector)){
+    stop("Input must be a character vector")
+  }
+  
+  # Read in results tables and return a list
+  # each item in the list is a list with 1) main results and 2) meta data
+  results <- lapply(full_path_vector, read_results)
+  
+  # add file name as attribute to each results table
+  # get basename without file extension
+  results_names <- file_path_sans_ext(basename(full_path_vector))
+
+  names(results) <- results_names
+
+  return(results)
+}
+
+# ---------------------------------------------
+# function that creates an excel spreadshseet 
+# it inserts a readme in the first sheet
+# and inserts tables in list of tables for all subsequent sheets
+# Create an excel spreadsheet with a new sheet for each file
+make_excel <- function(results, file_name, table_index, legend_title, legend_text,
+                        cell_title_width, cell_title_height){
+  wb <- createWorkbook()
+  
+  # Add a readme for the first sheet
+  add_readme(results, wb, table_index, legend_title, legend_text,
+             cell_title_width, cell_title_height)
+  
+  # Add each table to a new sheet
+  for (i in seq_along(results)) {
+    addWorksheet(wb, sheetName = names(results)[i])
+    writeData(wb, sheet = names(results)[i], results[[i]]$main, startRow = 1, colNames = TRUE)
+  }
+  
+  saveWorkbook(wb, file_name, overwrite = TRUE)
+}
+
+# ---------------------------------------------
+# function that creates README for first sheet in excel spreadsheet
+add_readme <- function(results, wb, table_index, legend_title, legend_text, 
+                       cell_title_width, cell_title_height){
   # Create a readme sheet
   addWorksheet(wb, sheetName = "README")
   
@@ -124,104 +158,65 @@ add_readme <- function(tables_list, wb, table_index, legend_title, legend_text,
   bold_style <- createStyle(textDecoration = "bold")
   addStyle(wb, sheet = "README", style = bold_style, rows = 1, cols = 1, stack = TRUE)
   
-  # Explain what the column names in each of the sheets mean
-  # for column names that are the same across sheets collapse these into one table
-  col_name_descriptions <- col_name_descriptions(tables_list, colname_descriptions)
+  # Each item in 'results' contains a 'meta' data frame describing column names 
+  # (from .cols sidecar files).
+  # Extract that meta data and combine them into a single data frame:
+  col_name_descriptions <- do.call(rbind, lapply(results, function(x) x$meta))
   
   writeData(wb, sheet = "README", col_name_descriptions, startRow = 3, startCol = 1)
   
   # make row with column name and descriptions bold
   addStyle(wb, sheet = "README", style = bold_style, rows = 3, cols = 1:2, stack = TRUE)
   
-  # # List the Supplementary Table Names:
-  # supplementary_tables <- paste("Supplementary Table ", table_index, LETTERS[1:length(tables_list)])
-  # 
-  # writeData(wb, sheet = "README", supplementary_tables, startRow = 3, startCol = 1)
-  # 
-  # # List the names of the tables_list next to the table number
-  # writeData(wb, sheet = "README", names(tables_list), startRow = 3, startCol = 2)
-  
 }
 
-# function that creates an excell spreadshseet 
-# it inserts a readme in the first sheet
-# and inserts tables in list of tables for all subsequent sheets
-# Create an excell spreadsheet with a new sheet for each file
-make_excell <- function(tables_list, file_name, table_index, legend_title, legend_text,
-                        cell_title_width, cell_title_height, colname_descriptions){
-  wb <- createWorkbook()
-  
-  # Add a readme for the first sheet
-  add_readme(tables_list, wb, table_index, legend_title, legend_text,
-             cell_title_width, cell_title_height, colname_descriptions)
-  
-  # Add each table to a new sheet
-  for (i in seq_along(tables_list)) {
-    addWorksheet(wb, sheetName = names(tables_list)[i])
-    writeData(wb, sheet = names(tables_list)[i], tables_list[[i]], startRow = 1, colNames = TRUE)
-  }
-  
-  saveWorkbook(wb, file_name, overwrite = TRUE)
-}
+###############################################
+#### Meta-analysis and fine mapping table #####
+###############################################
+create_table <- function(paths, regex, 
+                        excel_file_name, 
+                        table_index, 
+                        legend_title, legend_text,
+                        cell_title_width, cell_title_height){
 
-# Create the first supplementary table for the clumps and fine mapping results
-create_meta_finemapping <- function(excell_file_name, table_index, legend_title, legend_text,
-                                    cell_title_width, cell_title_height, colname_descriptions){
+  # Read in full paths for where results are stored
+  # get .csv/.tsv and sidecar .cols file (with column name descriptions)
+  files <- mapply(get_main_file_names, paths, regex) %>%
+            as.vector()
 
-  # Read in full paths for where clumps and fine mapping results are stored
-  clumps_path <- "manuscript/tables"
-  clumps_regex <- "antidep-2501.clumps"
-  clumps_files <- get_file_names(clumps_path,
-                           clumps_regex)
-  # split files into .csv and .cols
-  clumps <- str_subset(clumps_files, ".csv")
-  clumps_cols <- str_subset(clumps_files, ".cols")
-  
-  if(length(clumps) == 0){
-    stop(paste0("No files found at: ", clumps_path, " with regex: ", clumps_regex))
-  }
-  finemapping_path <- "manuscript/tables"
-  finemapping_regex <- "susiex_significant"
-  finemapping_files <- get_file_names(finemapping_path,
-                               finemapping_regex)
-  # split files into .csv and .cols
-  finemapping <- str_subset(clumps_files, ".csv")
-  finemapping_cols <- str_subset(clumps_files, ".cols")
-  
-  if(length(finemapping) == 0){
-    stop(paste0("No files found at: ", finemapping_path, " with regex: ", finemapping_regex))
-  }
-  
   # Read these tables and store as a list of data frames
+  # where each dataframe has an assocaited sidecar .cols file
   # the names of this list are the file names
-  results_list <- tables_list(c(clumps, finemapping))
+  results <- tables_list(files)
   
-  # Create colname_descriptions from the *_cols variable
-  colname_descriptions <- tables_list(c(clumps_cols, finemapping_cols)) %>%
-                            do.call(rbind, .)
-
-  # Make excell spreadsheet
-  make_excell(results_list, excell_file_name, table_index, legend_title, legend_text,
-              cell_title_width, cell_title_height, colname_descriptions)
+  # Make excel spreadsheet
+  make_excel(results, excel_file_name, table_index, legend_title, legend_text,
+              cell_title_width, cell_title_height)
   
 }
 
+###############################################
+#### Use main() to run create_table functions #
+###############################################
 main <- function(){
   # Set table_index to 1
   table_index <- update_table_index(0)
   
   # Create the first supplementary table for the clumps and fine mapping results
-  create_meta_finemapping(here::here(glue("manuscript/tables/S{table_index}_clumps_finemap.xlsx")),
-                          table_index,
-                          glue("Table S{table_index}. Clumping and fine mapping results for the meta-analysis of the antidepressant GWAS."),
-                          glue("Clumping results are divided into"),
-                          cell_title_width = 30,
-                          cell_title_height = 50)
+  create_table(paths = c("manuscript/tables"),
+               regex = c("susiex_significant"),
+               here::here(glue("manuscript/tables/S{table_index}_clumps_finemap.xlsx")),
+               table_index,
+               glue("Table S{table_index}. Clumping and fine mapping results for the meta-analysis of the antidepressant GWAS."),
+               glue("Clumping results are divided into"),
+               cell_title_width = 30,
+               cell_title_height = 50)
   
   # Set table index to 2
   table_index <- update_table_index(table_index)
 }
 
+# ---------------------------------------------
 # Run the main function
 main()
 
