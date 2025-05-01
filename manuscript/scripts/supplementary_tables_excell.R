70797dd57380e6dde3eb0cde360e88d587aefd6c
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-05-01 11:50:42 +0100

    run styler with default setting

--- manuscript/scripts/supplementary_tables_excell.R
+++ manuscript/scripts/supplementary_tables_excell.R
@@ -23,97 +23,113 @@ source(here("manuscript/scripts/supplementary_tables_excel_functions.R"))
 ###############################################
 #### Use main() to run create_table functions #
 ###############################################
-main <- function(){
+main <- function() {
   # Set table_index to 1
   table_index <- update_table_index(0)
-  
+
   # Create the first supplementary table for the clumps and fine mapping results
-  create_table(paths = rep("manuscript/tables", 4),
-               regex = c("clumps_fixed_antidep-2501.clumps",
-                         "clumps_mrmega_antidep-2501.clumps",
-                         "susiex_significant_summary",
-                         "susiex_significant_cs"),
-               sheet_names = c("clumps fixed",
-                               "clumps MR-MEGA",
-                               "SuSiEx summary",
-                               "SuSiEx credible sets"),
-               excel_file_name = here::here(glue("manuscript/tables/S{table_index}_clumps_finemap.xlsx")),
-               table_index,
-               legend_title = "Clumping and fine mapping results for the meta-analysis of the antidepressant GWAS.",
-               legend_text_prefix = "Results are divided into ",
-               legend_text_sections = c("fixed clumping results across all ancestries and antidepressant phenotypes",
-                                        "MR-MEGA clumping results",
-                                         "significant SuSiEx summary statistics",
-                                         "significant SuSiEx credible sets"),
-               cell_title_width = 30,
-               cell_title_height = 50)
-  
+  create_table(
+    paths = rep("manuscript/tables", 4),
+    regex = c(
+      "clumps_fixed_antidep-2501.clumps",
+      "clumps_mrmega_antidep-2501.clumps",
+      "susiex_significant_summary",
+      "susiex_significant_cs"
+    ),
+    sheet_names = c(
+      "clumps fixed",
+      "clumps MR-MEGA",
+      "SuSiEx summary",
+      "SuSiEx credible sets"
+    ),
+    excel_file_name = here::here(glue("manuscript/tables/S{table_index}_clumps_finemap.xlsx")),
+    table_index,
+    legend_title = "Clumping and fine mapping results for the meta-analysis of the antidepressant GWAS.",
+    legend_text_prefix = "Results are divided into ",
+    legend_text_sections = c(
+      "fixed clumping results across all ancestries and antidepressant phenotypes",
+      "MR-MEGA clumping results",
+      "significant SuSiEx summary statistics",
+      "significant SuSiEx credible sets"
+    ),
+    cell_title_width = 30,
+    cell_title_height = 50
+  )
+
   # Set table index to 2
   table_index <- update_table_index(table_index)
-  
+
   # Create the second supplementary table for the gene mapping results
-  create_table(paths = "manuscript/tables",
-               regex = c("mBAT-combo.csv"),
-               sheet_names = c("mBAT-combo"),
-               excel_file_name = here::here(glue("manuscript/tables/S{table_index}_gene_mapping.xlsx")),
-               table_index,
-               legend_title = "Positional mapping results for EUR, AFR and SAS fixed meta-analyses of the antidepressant GWAS (N06A, N06AA and N06AB).",
-               legend_text_prefix = "",
-               legend_text_sections = c("Results shown for Bonferroni corrected mBAT-combo p-value < 0.05"),
-               cell_title_width = 39,
-               cell_title_height = 49)
-  
+  create_table(
+    paths = "manuscript/tables",
+    regex = c("mBAT-combo.csv"),
+    sheet_names = c("mBAT-combo"),
+    excel_file_name = here::here(glue("manuscript/tables/S{table_index}_gene_mapping.xlsx")),
+    table_index,
+    legend_title = "Positional mapping results for EUR, AFR and SAS fixed meta-analyses of the antidepressant GWAS (N06A, N06AA and N06AB).",
+    legend_text_prefix = "",
+    legend_text_sections = c("Results shown for Bonferroni corrected mBAT-combo p-value < 0.05"),
+    cell_title_width = 39,
+    cell_title_height = 49
+  )
+
   # Set table index to 3
   table_index <- update_table_index(table_index)
-  
+
   # Create the second supplementary table for the GWAS catalog
-  create_table(paths = rep("manuscript/tables",11),
-               regex = c("gwascat_fixed_table_N06A-AFR.csv",
-                         "gwascat_fixed_table_N06A-EAS.csv",
-                         "gwascat_fixed_table_N06A-EUR.csv",
-                         "gwascat_fixed_table_N06A-SAS.csv",
-                         "gwascat_fixed_table_N06AA-AFR.csv",
-                         "gwascat_fixed_table_N06AA-EUR.csv",
-                         "gwascat_fixed_table_N06AB-AFR.csv",
-                         "gwascat_fixed_table_N06AB-EUR.csv",
-                         "gwascat_fixed_table_N06AB-MID.csv",
-                         "gwascat_fixed_table_N06AB-SAS.csv",
-                         "gwascat_mrmega_antidep-2501.csv"),
-               sheet_names = c("N06A-AFR",
-                               "N06A-EAS",
-                               "N06A-EUR",
-                               "N06A-SAS",
-                               "N06AA-AFR",
-                               "N06AA-EUR",
-                               "N06AB-AFR",
-                               "N06AB-EUR",
-                               "N06AB-MID",
-                               "N06AB-SAS",
-                               "MRMEGA"),
-               excel_file_name = here::here(glue("manuscript/tables/S{table_index}_gwas_cat.xlsx")),
-               table_index,
-               legend_title = "NHGRI-EBI GWAS catalogue lookup for the antidepressant meta-analysis GWAS",
-               legend_text_prefix = "Results split by ancestries and phenotypes for fixed and MRMEGA meta-analysis GWAS.",
-               legend_text_sections = c("N06A-AFR",
-                                        "N06A-EAS",
-                                        "N06A-EUR",
-                                        "N06A-SAS",
-                                        "N06AA-AFR",
-                                        "N06AA-EUR",
-                                        "N06AB-AFR",
-                                        "N06AB-EUR",
-                                        "N06AB-MID",
-                                        "N06AB-SAS",
-                                        "MRMEGA"),
-               cell_title_width = 39,
-               cell_title_height = 49)
-  
+  create_table(
+    paths = rep("manuscript/tables", 11),
+    regex = c(
+      "gwascat_fixed_table_N06A-AFR.csv",
+      "gwascat_fixed_table_N06A-EAS.csv",
+      "gwascat_fixed_table_N06A-EUR.csv",
+      "gwascat_fixed_table_N06A-SAS.csv",
+      "gwascat_fixed_table_N06AA-AFR.csv",
+      "gwascat_fixed_table_N06AA-EUR.csv",
+      "gwascat_fixed_table_N06AB-AFR.csv",
+      "gwascat_fixed_table_N06AB-EUR.csv",
+      "gwascat_fixed_table_N06AB-MID.csv",
+      "gwascat_fixed_table_N06AB-SAS.csv",
+      "gwascat_mrmega_antidep-2501.csv"
+    ),
+    sheet_names = c(
+      "N06A-AFR",
+      "N06A-EAS",
+      "N06A-EUR",
+      "N06A-SAS",
+      "N06AA-AFR",
+      "N06AA-EUR",
+      "N06AB-AFR",
+      "N06AB-EUR",
+      "N06AB-MID",
+      "N06AB-SAS",
+      "MRMEGA"
+    ),
+    excel_file_name = here::here(glue("manuscript/tables/S{table_index}_gwas_cat.xlsx")),
+    table_index,
+    legend_title = "NHGRI-EBI GWAS catalogue lookup for the antidepressant meta-analysis GWAS",
+    legend_text_prefix = "Results split by ancestries and phenotypes for fixed and MRMEGA meta-analysis GWAS.",
+    legend_text_sections = c(
+      "N06A-AFR",
+      "N06A-EAS",
+      "N06A-EUR",
+      "N06A-SAS",
+      "N06AA-AFR",
+      "N06AA-EUR",
+      "N06AB-AFR",
+      "N06AB-EUR",
+      "N06AB-MID",
+      "N06AB-SAS",
+      "MRMEGA"
+    ),
+    cell_title_width = 39,
+    cell_title_height = 49
+  )
+
   # Set table index to 4
   table_index <- update_table_index(table_index)
-  
 }
 
 # ---------------------------------------------
 # Run the main function
 main()
-
