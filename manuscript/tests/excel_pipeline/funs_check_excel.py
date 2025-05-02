be873ce2f0ba6d53772beefab9fb14dcfaa4da1d
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-05-02 17:28:31 +0100

    use xlwings as openpyxl could not open xlsx files generated in R. Test for cell contents and bold legend title

--- manuscript/tests/excel_pipeline/funs_check_excel.py
+++ manuscript/tests/excel_pipeline/funs_check_excel.py
@@ -1,5 +1,38 @@
 import os
+import xlwings as xw
 
+os.environ["XLWINGS_LICENSE_KEY"] = "noncommercial"
+
+# check excel file with test data exists
+# this file is created by running generate_test_data.R
 def check_file_exists(file_path: str) -> bool:
     """Check if the file exists."""
     return os.path.isfile(file_path)
+
+# check content of cells in the readme sheet exists
+# content should be in the following cells: row 1 & col 1, row 2 & col 1, row 3-10 & cols 1-3
+def check_readme_cell_contents_exist(file_path: str) -> bool:
+    """Check if the expected cells in the README sheet contain content"""
+    wb = xw.Book(file_path)
+    readme = wb.sheets[0]
+    cols_to_check = ['A1:A10' , 'B3:B10', 'C3:C10']
+    for col in cols_to_check:
+        value = readme.range(col).value
+        if value is None or value == '':
+            return False
+        else:
+            return True
+
+# check cell in row 1 and col 1 in first sheet is bold (this is the legend title)
+def check_legend_title_is_bold(file_path: str) -> bool:
+    """Check if the legend title in cell A1 is bold."""
+    wb = xw.Book(file_path)
+    legend = wb.sheets[0]['A1']
+    legend_is_bold = legend.font.bold
+    return legend_is_bold
+
+# check cell in row 2 and col 1 in first sheet has no style
+
+# check cells in row 3 and cols 1-3 are bold
+
+# check cells in row 3 and cols 1-3 have the words: sheet_name, column and description in each cell
--- manuscript/tests/excel_pipeline/tests_check_excel.py
+++ manuscript/tests/excel_pipeline/tests_check_excel.py
@@ -1,7 +1,26 @@
 
-from funs_check_excel import check_file_exists
 import pytest
+from funs_check_excel import (check_file_exists,
+                              check_readme_cell_contents_exist,
+                              check_legend_title_is_bold)
+
+file_path = "test_data/xlsx/S1_test_excel.xlsx"
 
 def test_sheets_exist():
-    file_path = "test_data/xlsx/S1_test_excel.xlsx"
     assert check_file_exists(file_path) is True
+
+def test_readme_cell_contents_exist():
+    assert check_readme_cell_contents_exist(file_path) is True
+
+# check content of cells in the readme sheet exists
+# content should be in the following cells: row 1 & col 1, row 2 & col 1, row 3-10 & cols 1-3
+
+# check cell in row 1 and col 1 in first sheet is bold (this is the legend title)
+def test_legend_title_is_bold():
+    assert check_legend_title_is_bold(file_path) is True
+
+# check cell in row 2 and col 1 in first sheet has no style
+
+# check cells in row 3 and cols 1-3 are bold
+
+# check cells in row 3 and cols 1-3 have the words: sheet_name, column and description in each cell
