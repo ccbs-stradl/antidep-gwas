a6c9589e8e989df1335c4bcb73eea1a9e6f14cd8
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-05-06 17:53:20 +0100

    update smr script to create placeholders for description table

--- manuscript/scripts/smr.R
+++ manuscript/scripts/smr.R
@@ -6,37 +6,38 @@ library(tidyr)
 library(stringr)
 
 # Create col name descriptions, same for all SMR results
-# colname_descriptions <- 
-# c(index
-# qtl_name
-# probeID
-# ProbeChr
-# Probe_bp
-# topSNP
-# topSNP_chr
-# topSNP_bp
-# A1
-# A2
-# Freq
-# b_GWAS
-# se_GWAS
-# p_GWAS
-# b_eQTL
-# se_eQTL
-# p_eQTL
-# b_SMR
-# se_SMR
-# p_SMR
-# p_HEIDI
-# nsnp_HEIDI
-# gene_id
-# chr
-# start
-# end
-# strand
-# GWAS_LOCUS
-# Lead_SNP
-# Lead_SNP_BP)
+colname_descriptions <-
+c("Gene" = "placeholder",
+"qtl_name" = "placeholder",
+"probeID" = "placeholder",
+"ProbeChr" = "placeholder",
+"Probe_bp" = "placeholder",
+"topSNP" = "placeholder",
+"topSNP_chr" = "placeholder",
+"topSNP_bp" = "placeholder",
+"A1" = "placeholder",
+"A2" = "placeholder",
+"Freq" = "placeholder",
+"b_GWAS" = "placeholder",
+"se_GWAS" = "placeholder",
+"p_GWAS" = "placeholder",
+"b_eQTL" = "placeholder",
+"se_eQTL" = "placeholder",
+"p_eQTL" = "placeholder",
+"b_SMR" = "placeholder",
+"se_SMR" = "placeholder",
+"p_SMR" = "placeholder",
+"p_HEIDI" = "placeholder",
+"nsnp_HEIDI" = "placeholder",
+"gene_id" = "placeholder",
+"chr" = "placeholder",
+"start" = "placeholder",
+"end" = "placeholder",
+"strand" = "placeholder",
+"GWAS_LOCUS" = "placeholder",
+"Lead_SNP" = "placeholder",
+"Lead_SNP_BP" = "placeholder"
+)
 
 # Load function to create .cols sidecar meta data file
 source(here::here("manuscript/scripts/supplementary_tables_excell_create_cols_meta_FUN.R"))
